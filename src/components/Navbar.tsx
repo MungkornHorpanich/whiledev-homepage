37c3f8bce26866137562a4fbcf744c5c388b5b2a
M <MungkornHorpanich@gmail.com>
2024-12-08 11:43:45 +0700

    adding headlessui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../public/logo.png";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="w-screen mx-auto fixed backdrop-blur-md">
+    <div className="w-full bg-[#202023] mx-auto backdrop-blur-md mt-10">
       <div className="mx-auto max-w-3xl">
-        <div className="text-white flex flex-row p-3 pl-4">
+        <div className="text-white flex flex-row ml-5 md:m-0 p-3 pl-4 gap-3">
           <Link to={"/"}>
-            <div className="flex">
-              <div className="flex text-lg text-center font-semibold">
-                WhileDev
-              </div>
-            </div>
+            <img src={logo} alt="logo" className="w-20 h-20 rounded-3xl " />
           </Link>
-          <ul className="flex text-center text-lg pl-12">
-            <li>hello</li>
-            <li>xd</li>
-          </ul>
+          <div className="hidden">
+            <div className="flex text-2xl text-center font-bold tracking-wide font-mono text-gray-200">
+              whiledev.
+            </div>
+            <ul className="flex text-center text-lg ">
+              <li>hello</li>
+              <li>xd</li>
+            </ul>
+          </div>
+
+          {/* The burger menu */}
         </div>
       </div>
     </div>
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 const Homepage: React.FC = () => {
   return (
-    <div>
+    <div className="bg-[#202023]">
       <Navbar />
     </div>
   );
