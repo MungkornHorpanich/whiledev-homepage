561fe5a566ff6d1c44f9096f970a82031d9ad355
M <MungkornHorpanich@gmail.com>
2024-12-16 12:42:13 +0700

    adding loading effect

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface CardProps {
@@ -18,16 +18,37 @@ const Card: React.FC<CardProps> = ({
   style,
   catergory,
 }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const handleLoad = () => {
+    setIsLoaded(true);
+  };
+
+  const handleError = () => {
+    setHasError(true);
+  };
+
   return (
     <div className="p-5 border border-gray-200 dark:border-[#2e2e2e] rounded-xl">
       <div className={`md:px-0 flex flex-col ${style}`}>
-        <Link to={`/posts/${path}`}>
-          <img
-            src={image}
-            className="rounded-xl md:hover:scale-105 w-full transition duration-300 ease-in-out cursor-pointer"
-            alt="hello"
-          />
-        </Link>
+        <div
+          className={`${
+            !isLoaded ? "bg-gray-200" : ""
+          } transition-all duration-300 `}
+        >
+          {!isLoaded && <p className="text-center text-gray-500"></p>}{" "}
+          {/* Show loading message */}
+          <Link to={`/posts/${path}`}>
+            <img
+              src={image}
+              className="rounded-xl md:hover:scale-105 w-full transition duration-300 ease-in-out cursor-pointer"
+              alt="hello"
+              onLoad={handleLoad}
+              onError={handleError}
+            />
+          </Link>
+        </div>
         <Link
           to={"/posts/react-tutorial"}
           className="text-lg md:text-2xl pt-3 font-semibold px-3"
