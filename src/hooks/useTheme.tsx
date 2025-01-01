cb5d5222f88693a95187f750d197a3bb433c4700
M <MungkornHorpanich@gmail.com>
2025-01-01 13:50:14 +0700

    removing comment

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -3,20 +3,18 @@ import { useState, useEffect } from "react";
 const useTheme = () => {
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem("theme");
-    return savedTheme || "light"; // Default to 'light' if no saved theme
+    return savedTheme || "light";
   });
 
   useEffect(() => {
     const root = document.documentElement;
 
-    // Ensure that theme is correctly applied to the root element
     if (theme === "dark") {
       root.classList.add("dark");
     } else {
       root.classList.remove("dark");
     }
 
-    // Store the theme in localStorage so it persists across sessions
     localStorage.setItem("theme", theme);
   }, [theme]);
 
