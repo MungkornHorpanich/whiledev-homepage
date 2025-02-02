4f5a37be51ef5c6ea3e10989cfd19cb5aed3567c
M <MungkornHorpanich@gmail.com>
2025-02-02 17:34:51 +0700

    changing the footer to 2026 instead of 2024

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <div className="text-sm dark:bg-[#202023] font-medium text-gray-500 dark:text-gray-400 text-center py-10 px-3">
-      © 2024 Jiranthanin Horpanich. All Rights Reserved
+      © 2026 Jiranthanin Horpanich. All Rights Reserved
     </div>
   );
 };
