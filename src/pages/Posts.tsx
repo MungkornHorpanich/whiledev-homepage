fb695163dba27fd25e4ff925dd83faffcaac61d1
M <MungkornHorpanich@gmail.com>
2025-05-03 22:18:51 +0700

    fixing padding for the post and work page

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -6,54 +6,54 @@ import NextApollo from "../../public/next-apollo.png";
 import MacOS from "../../public/macOS.png";
 
 const Posts: React.FC = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, []);
 
-  return (
-    <div className="dark:bg-[#202023] transition duration-400 ease-in-out [--anchor-gap:var(--spacing-5)] min-h-screen bg-white overflow-auto">
-      <Navbar />
-      <div className="pt-20 max-w-4xl mx-auto pl-5 text-black dark:text-white">
-        <h1 className="text-xl md:text-2xl font-medium">Posts</h1>
-        <div className="grid text-black dark:text-white grid-cols-1 gap-3 mt-4 md:grid-cols-2 md:px-3 md:py-2">
-          <Card
-            image={NextApollo}
-            title="Why GraphQL with Apollo Server Feels Like Overkill for Smaller SaaS Projects and Next.js is better for SaaS."
-            description="In this post I'm covering why I switched from GraphQL, Apollo Server with TypeORM to Next. js with Prisma"
-            path="next-vs-apollo"
-            catergory={[
-              "react",
-              "next.js",
-              "typescript",
-              "graphql",
-              "apollo-server",
-              "prisma",
-              "typeorm",
-            ]}
-          />
+   return (
+      <div className="dark:bg-[#202023] transition duration-400 ease-in-out [--anchor-gap:var(--spacing-5)] min-h-screen bg-white overflow-auto">
+         <Navbar />
+         <div className="pt-20 max-w-4xl mx-auto md:px-0 px-5 text-black dark:text-white">
+            <h1 className="text-xl md:text-2xl font-medium">Posts</h1>
+            <div className="grid text-black dark:text-white grid-cols-1 gap-3 mt-4 md:grid-cols-2 md:px-3 md:py-2">
+               <Card
+                  image={NextApollo}
+                  title="Why GraphQL with Apollo Server Feels Like Overkill for Smaller SaaS Projects and Next.js is better for SaaS."
+                  description="In this post I'm covering why I switched from GraphQL, Apollo Server with TypeORM to Next. js with Prisma"
+                  path="next-vs-apollo"
+                  catergory={[
+                     "react",
+                     "next.js",
+                     "typescript",
+                     "graphql",
+                     "apollo-server",
+                     "prisma",
+                     "typeorm",
+                  ]}
+               />
 
-          <Card
-            image={MacOS}
-            title="The Ultimate macOS Workflow for Power Users: My Tools and Setup"
-            description="Switching from Yabai to Aerospace has transformed my macOS workflow. In this post, I explain how Aerospace, combined with Ghostty Terminal, Tmux, and SketchyBar, creates a powerful and efficient setup for maximizing productivity."
-            path="macos-workflow"
-            catergory={[
-              "macos",
-              "window management",
-              "productivity",
-              "aerospace",
-              "terminal",
-              "tmux",
-              "sketchybar",
-              "ghostty",
-            ]}
-          />
-        </div>
+               <Card
+                  image={MacOS}
+                  title="The Ultimate macOS Workflow for Power Users: My Tools and Setup"
+                  description="Switching from Yabai to Aerospace has transformed my macOS workflow. In this post, I explain how Aerospace, combined with Ghostty Terminal, Tmux, and SketchyBar, creates a powerful and efficient setup for maximizing productivity."
+                  path="macos-workflow"
+                  catergory={[
+                     "macos",
+                     "window management",
+                     "productivity",
+                     "aerospace",
+                     "terminal",
+                     "tmux",
+                     "sketchybar",
+                     "ghostty",
+                  ]}
+               />
+            </div>
 
-        <Footer />
+            <Footer />
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Posts;
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -7,33 +7,33 @@ import edgegambit from "../../public/edgambit.png";
 import ChatBubble from "../../public/chat-bubble.png";
 
 const Work: React.FC = () => {
-  return (
-    <div className="dark:bg-[#202023] transition text-black dark:text-white duration-400 ease-in-out [--anchor-gap:var(--spacing-5)] min-h-screen bg-white ">
-      <Navbar />
-      <div className="max-w-4xl mx-auto pt-20">
-        <Link to={"/work"} className="flex px-5 gap-2 hover:underline">
-          <h1 className="text-xl md:text-2xl font-medium">Works</h1>
-        </Link>
-        <div className="grid grid-cols-1 gap-3 mt-4 md:grid-cols-2 md:px-3 md:py-2">
-          <Carda
-            image={edgegambit}
-            title="EdgeGambit"
-            description="New concept of Trading Competitions, A trading competitions platform."
-            path="https://edgegambit.vercel.app/"
-            tech={["typescirpt", "react", "tailwindcss"]}
-          />
-          <Carda
-            image={ChatBubble}
-            title="Chat-Bubble Effect"
-            description="Simple Chat-Bubble effect app"
-            path="https://whiledev-chat-bubble.vercel.app/"
-            tech={["typescirpt", "react", "tailwindcss"]}
-          />
-        </div>
+   return (
+      <div className="dark:bg-[#202023] transition text-black dark:text-white duration-400 ease-in-out [--anchor-gap:var(--spacing-5)] min-h-screen bg-white ">
+         <Navbar />
+         <div className="max-w-4xl px-5 md:px-0 mx-auto pt-20">
+            <Link to={"/work"} className="flex px-5 gap-2 hover:underline">
+               <h1 className="text-xl md:text-2xl font-medium">Works</h1>
+            </Link>
+            <div className="grid grid-cols-1 gap-3 mt-4 md:grid-cols-2 md:px-3 md:py-2">
+               <Carda
+                  image={edgegambit}
+                  title="EdgeGambit"
+                  description="New concept of Trading Competitions, A trading competitions platform."
+                  path="https://edgegambit.vercel.app/"
+                  tech={["typescirpt", "react", "tailwindcss"]}
+               />
+               <Carda
+                  image={ChatBubble}
+                  title="Chat-Bubble Effect"
+                  description="Simple Chat-Bubble effect app"
+                  path="https://whiledev-chat-bubble.vercel.app/"
+                  tech={["typescirpt", "react", "tailwindcss"]}
+               />
+            </div>
+         </div>
+         <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+   );
 };
 
 export default Work;
