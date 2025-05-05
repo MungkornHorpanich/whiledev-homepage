82750030e4adfa69ae2e4eaf8bdfaf96a0cf4186
M <MungkornHorpanich@gmail.com>
2025-05-05 22:26:50 +0700

    xd

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,36 @@ import Posts from "./pages/Posts";
 import NextVSApollo from "./pages/posts/NextVSApollo";
 import MacosWorkflow from "./pages/posts/MacosWorkflow";
 
+// This shit is dead bro fr no post for decades
 const router = createHashRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/work",
-    element: <Work />,
-  },
-  {
-    path: "/posts",
-    element: <Posts />,
-  },
-  {
-    path: "/posts/next-vs-apollo",
-    element: <NextVSApollo />,
-  },
-  {
-    path: "/posts/macos-workflow",
-    element: <MacosWorkflow />,
-  },
+   {
+      path: "/",
+      element: <Homepage />,
+   },
+   {
+      path: "/work",
+      element: <Work />,
+   },
+   {
+      path: "/posts",
+      element: <Posts />,
+   },
+   {
+      path: "/posts/next-vs-apollo",
+      element: <NextVSApollo />,
+   },
+   {
+      path: "/posts/macos-workflow",
+      element: <MacosWorkflow />,
+   },
 ]);
 
 const App: React.FC = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+   return (
+      <>
+         <RouterProvider router={router} />
+      </>
+   );
 };
 
 export default App;
