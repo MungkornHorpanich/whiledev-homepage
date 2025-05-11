d9101362ea7b56d31063d9f5cec04369a4e2707b
M <MungkornHorpanich@gmail.com>
2025-05-11 10:01:53 +0700

    xd

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// Hello
+// Hello xd
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
